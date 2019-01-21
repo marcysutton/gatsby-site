affd8d62a5a864a33fd80d720c4a337a4f938a96
Marcy Sutton <marcy.sutton@deque.com>
2019-01-21 00:56:00 -0800

    make more-link into "all [posts]" link

--- src/components/more-link.js
+++ src/components/more-link.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const MoreLink = ({ itemLabel }) => (
     <p className="more-link">
-        <a href={ `/${ itemLabel }` }>More { itemLabel }</a>
-        <span aria-hidden="true"> ▾</span>
+        <a href={ `/${ itemLabel }` }>
+            <span className="more-link-text">All { itemLabel }</span>
+            <span aria-hidden="true"> ▶</span>
+        </a>
+        {/* <span aria-hidden="true"> ▾</span> */}
     </p>
 )
 
