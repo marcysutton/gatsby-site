2a4a14f3eb39a452af54ac87b744a63508eaab72
Marcy Sutton <marcy.sutton@deque.com>
2019-01-21 00:55:08 -0800

    add cool magazine image to links page

--- src/pages/links.js
+++ src/pages/links.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
 import BodyClassName from 'react-body-classname'
 
 import Layout from '../components/layout'
@@ -12,6 +13,17 @@ const LinksPage = ({ data }) => (
             <SEO title="Links" keywords={['Marcy Sutton', 'links', 'web developer']} />
             <section className="generic-wrap">
                 <h1>Links</h1>
+                <Img
+                    style={{
+                        border: `1px solid #ccc`,
+                        marginBottom: `1em`,
+                        marginLeft: `2em`,
+                        float: `right`,
+                        width: `50%`
+                    }}
+                    fluid={data.linkPageImage.childImageSharp.fluid}
+                    alt="Netmag layout: Marcy emceeing CascadiaFest in a Canadian Mounty costume with Matthew Claypotch on stage in a horse head"
+                />
                 <p>A list of blog posts and podcasts on other sites, either written by me or referencing me somehow. All links open in new windows.</p>
                 <List 
                     className="list-links no-background"
@@ -34,7 +46,14 @@ export const pageQuery = graphql`
             title
           }
         }
-      }
+    }
+    linkPageImage: file(relativePath: { eq: "cascadiafest-netmag.PNG" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+    }
   }
 `
 
