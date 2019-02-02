1f6f8733f8ff037e28082425e885a7b01931ee6f
Marcy Sutton <marcy.sutton@deque.com>
2019-02-01 20:47:07 -0800

    mobile style tweaks

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,7 +2,7 @@ import Typography from "typography"
 
 const typography = new Typography({
   baseFontSize: "18px",
-  baseLineHeight: 1.666,
+  baseLineHeight: 1.25,
   headerFontFamily: [
     "itc-fenice",
     "Times New Roman",
