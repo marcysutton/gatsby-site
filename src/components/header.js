8654bbadd47b084a7253a8c5bb3587b3c9a3524a
Marcy Sutton <marcy.sutton@deque.com>
2019-02-06 10:37:22 -0800

    fix navigation with noscript

--- src/components/header.js
+++ src/components/header.js
@@ -24,35 +24,35 @@ const Header = ({ pathname }) => (
             role="banner"
           >
             <BackgroundImage className="header-bg" fluid={imageData}>
-            <ul className="skip-links">
-              <li><a href="#main" id="skip-link-main">Skip to main content</a></li>
-            </ul>
-            <div>
-              <h1 className="site-logo" style={{ margin: 0 }}>
-                <Link to="/" id="top" tabIndex="-1"
-                >
-                  <span className="logotype-line1">Marcy</span>
-                  <span className="logotype-line2">Sutton</span>
-                  <span className="logotype-line3">.com</span>
-                </Link>
-              </h1>
-              <nav>
-                <ul>
-                  <li><Link to="/" activeClassName="active">Home</Link></li>
-                  <li><Link to="/writing/" activeClassName="active" className={pathname && pathname.indexOf('post') > -1 ? 'active' : null}>
-                    Writing</Link>
-                  </li>
-                  <li>
-                    <Link to="/talks/" activeClassName="active" className={pathname && pathname.indexOf('talk') > -1 ? 'active' : null}>
-                    Talks</Link>
-                  </li>
-                  <li><Link to="/links/" activeClassName="active">Links</Link></li>
-                  <li><Link to="/about/" activeClassName="active">About</Link></li>
-                  <li><Link to="/contact/" activeClassName="active">Contact</Link></li>
-                  {/* <li><Link to="/search/" activeClassName="active">Search</Link></li> */}
-                </ul>
-              </nav>
-            </div>
+              <ul className="skip-links">
+                <li><a href="#main" id="skip-link-main">Skip to main content</a></li>
+              </ul>
+              <div style={{position: `relative`}}>
+                <h1 className="site-logo" style={{ margin: 0 }}>
+                  <Link to="/" id="top" tabIndex="-1"
+                  >
+                    <span className="logotype-line1">Marcy</span>
+                    <span className="logotype-line2">Sutton</span>
+                    <span className="logotype-line3">.com</span>
+                  </Link>
+                </h1>
+                <nav>
+                  <ul>
+                    <li><Link to="/" activeClassName="active">Home</Link></li>
+                    <li><Link to="/writing/" activeClassName="active" className={pathname && pathname.indexOf('post') > -1 ? 'active' : null}>
+                      Writing</Link>
+                    </li>
+                    <li>
+                      <Link to="/talks/" activeClassName="active" className={pathname && pathname.indexOf('talk') > -1 ? 'active' : null}>
+                      Talks</Link>
+                    </li>
+                    <li><Link to="/links/" activeClassName="active">Links</Link></li>
+                    <li><Link to="/about/" activeClassName="active">About</Link></li>
+                    <li><Link to="/contact/" activeClassName="active">Contact</Link></li>
+                    {/* <li><Link to="/search/" activeClassName="active">Search</Link></li> */}
+                  </ul>
+                </nav>
+              </div>
             </BackgroundImage>
           </header>
        )
