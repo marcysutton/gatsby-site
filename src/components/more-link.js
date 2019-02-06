1a6923c47d755f842b0999e576943164113c1fca
Marcy Sutton <marcy.sutton@deque.com>
2019-02-05 21:30:41 -0800

    prevent iOS from rendering emoji on more link

--- src/components/more-link.js
+++ src/components/more-link.js
@@ -4,7 +4,7 @@ const MoreLink = ({ itemLabel }) => (
     <p className="more-link">
         <a href={ `/${ itemLabel }` }>
             <span className="more-link-text">All { itemLabel }</span>
-            <span aria-hidden="true"> ▶</span>
+            <span aria-hidden="true"> ▶&#xFE0E;</span>
         </a>
         {/* <span aria-hidden="true"> ▾</span> */}
     </p>
