2ffabc9039fa85af796c389fe0d9c52a048a4c7e
Marcy Sutton <marcy.sutton@deque.com>
2019-02-05 17:25:38 -0800

    add 404 page easter egg

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,14 +3,17 @@ import BodyClassName from 'react-body-classname'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import Video from '../components/video'
 
 const NotFoundPage = () => (
   <BodyClassName className="page">
     <Layout>
       <SEO title="404: Not found" />
-      <section className="generic-wrap">
+      <section className="generic-wrap breathing-room">
         <h1>NOT FOUND</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <p>You just hit a page that doesn&#39;t exist... the sadness.</p>
+        <p>May I suggest a video instead?</p>
+        <Video videoSrcURL="https://www.youtube-nocookie.com/embed/iJ4T9CQA0UM" videoTitle="Guy on a Buffalo on YouTube" />
       </section>
     </Layout>
   </BodyClassName>
