f60f77cd046922122825f9dd6cf7611eeedce760
Marcy Sutton <marcy.sutton@deque.com>
2019-02-16 00:05:05 -0800

    add rss feed functionality

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,6 +9,33 @@ const Promise = require(`bluebird`)
 const path = require(`path`)
 const slash = require(`slash`)
 
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const fileNode = getNode(node.parent)
+    let nodeSlug
+    nodeSlug = ensureSlashes(
+      path.basename(fileNode.relativePath, path.extname(fileNode.relativePath))
+    )
+    if (nodeSlug) {
+      createNodeField({ node, name: `slug`, value: nodeSlug })
+    }
+  }
+}
+
+function ensureSlashes(slug) {
+  if (slug.charAt(0) !== `/`) {
+    slug = `/` + slug
+  }
+
+  if (slug.charAt(slug.length - 1) !== `/`) {
+    slug = slug + `/`
+  }
+
+  return slug
+}
+
 // Will create pages for talks (route : /talk/{slug})
 // Will create pages for posts (route : /{slug})
 exports.createPages = ({ graphql, actions }) => {
