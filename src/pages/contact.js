8baa67371e3fda58cc91c286b7ae15882453bd00
Marcy Sutton <marcy.sutton@deque.com>
2019-02-15 23:31:38 -0800

    enable contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,9 +13,9 @@ const ContactPage = ({ data }) => (
                 <section className="page-post-detail contact-detail">
                     <h1>Holla Back</h1>
                     <div>
-                        <p>This form is temporarily disabled while I migrate my website; please contact me <a href="https://twitter.com/marcysutton" target="_blank" rel="noopener noreferrer" title="Link opens in a new window">on Twitter</a> in the meantime. Thank you for your patience!</p>
-                        {/* <p><em>Want to get in touch? Fill out this form, and I’ll do my best to get back to you.</em></p>
-                        <p><strong>All fields are required.</strong></p> */}
+                        {/* <p>This form is temporarily disabled while I migrate my website; please contact me <a href="https://twitter.com/marcysutton" target="_blank" rel="noopener noreferrer" title="Link opens in a new window">on Twitter</a> in the meantime. Thank you for your patience!</p> */}
+                        <p><em>Want to get in touch? Fill out this form, and I’ll do my best to get back to you.</em></p>
+                        <p><strong>All fields are required.</strong></p>
                         <div role="form" lang="en-US" dir="ltr">
                             <div className="screen-reader-response"></div>
                             <form disabled key="contact-form" action="/thanks/" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="pooh-field">
@@ -25,22 +25,22 @@ const ContactPage = ({ data }) => (
                                 <div className="response-output display-none"></div>
                                 <p>
                                     <label htmlFor="name">Your Name</label><br />
-                                    <input disabled type="text" name="name" size="40" id="name" required="true" />
+                                    <input type="text" name="name" size="40" id="name" required="true" />
                                 </p>
                                 <p>
                                     <label htmlFor="email">Your Email</label><br />
-                                    <input disabled type="email" name="email" size="40" id="email" required="true" />
+                                    <input type="email" name="email" size="40" id="email" required="true" />
                                 </p>
                                 <p>
                                     <label htmlFor="subject">Your Subject</label><br />
-                                    <input disabled type="text" name="subject" size="40" id="subject" required="true" />
+                                    <input type="text" name="subject" size="40" id="subject" required="true" />
                                 </p>
                                 <p>
                                     <label htmlFor="message">Your Message</label><br />
                                     <textarea disabled name="message" cols="40" rows="10" id="message" required="true"></textarea>
                                 </p>
-                                <p><input disabled type="submit" value="Send" /></p>
-                                <input disabled type="hidden" name="form-name" value="contact" />
+                                <p><input type="submit" value="Send" /></p>
+                                <input type="hidden" name="form-name" value="contact" />
                             </form>
                         </div>
                     </div>
