fed3f88ce20d88267750fb8d956e823bd95da34c
Marcy Sutton <marcy.sutton@deque.com>
2019-04-13 23:43:43 -0700

    update talks

--- src/components/talk-events.js
+++ src/components/talk-events.js
@@ -4,59 +4,65 @@ const UpcomingTalks = () => (
     <>
         <h2>Upcoming Talks</h2>
         <ul id="upcoming-talks">
-        <li className="offsite-link">
-            <a href="http://www.csun.edu/cod/conference/2019/sessions/index.php/" target="_blank" rel="noopener noreferrer">
-                <span className="inline-link">CSUN Assistive Tech Conference</span>
-            </a><br />
-            Anaheim, CA
-            March 2019</li>
-        <li className="offsite-link">
-            <a href="https://events.drupal.org/seattle2019/builder" target="_blank" rel="noopener noreferrer">
-                <span className="inline-link">DrupalCon</span>
-            </a><br />
-            Seattle, WA
-            April 2019</li>
-        <li className="offsite-link">
-            <a title="Link opens in a new window" href="https://reactjsgirls.com/" target="_blank" rel="noopener noreferrer">
-                <span className="inline-link">React.js Girls</span>
-            </a><br />
-            London, UK
-            May 2019</li>
-        <li className="offsite-link">
-            <a title="Link opens in a new window" href="https://aneventapart.com/event/boston-2019" target="_blank" rel="noopener noreferrer">
-                <span className="inline-link">An Event Apart</span>
-            </a><br />
-            Boston, MA
-            May 2019</li>
-        <li className="offsite-link">
-            <a title="Link opens in a new window" href="https://frontendmasters.com/" target="_blank" rel="noopener noreferrer">
-                <span className="inline-link">Frontend Masters</span>
-            </a><br />
-            Minneapolis, MN
-            July 2019</li>
-        <li className="offsite-link">
-            <a href="https://aneventapart.com/event/chicago-2019" target="_blank" rel="noopener noreferrer">
-                <span className="inline-link">An Event Apart</span>
-            </a><br />
-            Chicago, IL
-            August 2019</li>
-        <li className="offsite-link">
-            <a href="https://www.meetup.com/a11ychi/" target="_blank" rel="noopener noreferrer">
-                <span className="inline-link">Accessibility Chicago Meetup</span>
-            </a><br />
-            August 2019</li>
-        <li className="offsite-link">
-            <a title="Link opens in a new window" href="https://www.paris-web.fr/" target="_blank" rel="noopener noreferrer">
-                <span className="inline-link">ParisWeb</span>
-            </a><br />
-            France
-            October, 2019</li>
-        <li className="offsite-link">
-            <a title="Link opens in a new window" href="https://smashingconf.com/ny-2019/" target="_blank" rel="noopener noreferrer">
-                <span className="inline-link">SmashingConf</span>
-            </a><br />
-            New York, NY
-            October, 2019</li>
+            <li className="offsite-link">
+                <a title="Link opens in a new window" href="https://reactjsgirls.com/" target="_blank" rel="noopener noreferrer">
+                    <span className="inline-link">React.js Girls</span>
+                </a><br />
+                London, UK
+                May 2019</li>
+            <li className="offsite-link">
+                <a title="Link opens in a new window" href="https://www.meetup.com/London-Accessibility-Meetup/" target="_blank" rel="noopener noreferrer">
+                    <span className="inline-link">London A11y Meetup</span>
+                </a><br />
+                London, UK
+                May 2019</li>
+            <li className="offsite-link">
+                <a title="Link opens in a new window" href="https://aneventapart.com/event/boston-2019" target="_blank" rel="noopener noreferrer">
+                    <span className="inline-link">An Event Apart</span>
+                </a><br />
+                Boston, MA
+                May 2019</li>
+            <li className="offsite-link">
+                <a title="Link opens in a new window" href="https://frontendmasters.com/" target="_blank" rel="noopener noreferrer">
+                    <span className="inline-link">Frontend Masters</span>
+                </a><br />
+                Minneapolis, MN
+                July 2019</li>
+            <li className="offsite-link">
+                <a href="https://clarityconf.com/2019" target="_blank" rel="noopener noreferrer">
+                    <span className="inline-link">Clarity</span>
+                </a><br />
+                Chicago, IL
+                August 2019</li>
+            <li className="offsite-link">
+                <a href="https://aneventapart.com/event/chicago-2019" target="_blank" rel="noopener noreferrer">
+                    <span className="inline-link">An Event Apart</span>
+                </a><br />
+                Chicago, IL
+                August 2019</li>
+            <li className="offsite-link">
+                <a href="https://www.meetup.com/a11ychi/" target="_blank" rel="noopener noreferrer">
+                    <span className="inline-link">Accessibility Chicago Meetup</span>
+                </a><br />
+                August 2019</li>
+            <li className="offsite-link">
+                <a title="Link opens in a new window" href="https://www.paris-web.fr/" target="_blank" rel="noopener noreferrer">
+                    <span className="inline-link">ParisWeb</span>
+                </a><br />
+                France
+                October, 2019</li>
+            <li className="offsite-link">
+                <a title="Link opens in a new window" href="https://smashingconf.com/ny-2019/" target="_blank" rel="noopener noreferrer">
+                    <span className="inline-link">SmashingConf</span>
+                </a><br />
+                New York, NY
+                October, 2019</li>
+            <li className="offsite-link">
+                <a title="Link opens in a new window" href="https://conf.a11yto.com/" target="_blank" rel="noopener noreferrer">
+                    <span className="inline-link">A11yTO Conf</span>
+                </a><br />
+                Toronto, Canada
+                October, 2019</li>
         </ul>
     </>
 )
