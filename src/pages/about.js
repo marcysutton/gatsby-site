6c5c980a1c807e4aef42fff18663414be7856aa2
Marcy Sutton <marcy@gatsbyjs.com>
2020-07-16 17:30:29 -0700

    update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,14 +17,7 @@ const AboutPage = () => (
                     }
                 }
             }
-            biking: file(relativePath: { eq: "anderson-mtn.jpg" }) {
-                childImageSharp {
-                    fluid {
-                        ...GatsbyImageSharpFluid
-                    }
-                }
-            }
-            life: file(relativePath: { eq: "van-love.jpg" }) {
+            biking: file(relativePath: { eq: "rambod.jpg" }) {
                 childImageSharp {
                     fluid {
                         ...GatsbyImageSharpFluid
@@ -49,7 +42,7 @@ const AboutPage = () => (
 
                             <p>My education includes a Bachelor of Arts in Visual Journalism from Brooks Institute of Photography and an Associate of Applied Arts in Web Design &#038; Multimedia from the Art Institute of Seattle. I became very interested in web development during college, creating HTML websites for photo documentary classes and later, <a href="https://marcy.codes/v2/" target="_blank" rel="noopener noreferrer" title="Link opens in a new window">interactive Flash websites</a>. I naturally went back to HTML &amp; CSS, with ECMAScript experience that helped me fall in love with JavaScript. When I learned about Web Accessibility, I liked that it reflected my values and added meaning to my work. I love solving problems with design and code when they have potential to help as many people as possible.</p>
 
-                            <p>I'm inspired by nature, street art, typography, and music, and I try to appreciate life's ups and downs. I like cooking and baking pies, and going places in our <a href="https://www.instagram.com/explore/tags/icebergthebeefy/" target="_blank" rel="noopener noreferrer" title="Link opens in a new window">offroad van</a>. I'm also pretty hooked on <a href="https://instagram.com/marcysutton" target="_blank" rel="noopener noreferrer" title="Link opens in a new window">riding bikes and playing outside</a>.</p>
+                            <p>I'm inspired by nature, street art, typography, and music, and I try to appreciate life's ups and downs. I like cooking and baking pies, and doing night photography. I'm also pretty hooked on <a href="https://instagram.com/marcysutton" target="_blank" rel="noopener noreferrer" title="Link opens in a new window">riding bikes and playing outside</a>.</p>
 
                             <h2 id="bio-short-version">Bio: Short Version</h2>
                             <p>Marcy Sutton is a Lead Engineer, Developer Relations at Gatsby, a startup creating fast, accessible websites and web apps while building a strong community. <span role="img" aria-label="heart">💜</span> Previously, she worked on the open source axe-core accessibility testing library. In 2016, O’Reilly gave Marcy a Web Platform Award for her work in accessibility. She co-leads the Accessibility Seattle and NW Tech Women meetups in her local region. When away from the keyboard, Marcy can be found hiking with her dog, riding a bicycle, or snowboarding.</p>
@@ -62,7 +55,6 @@ const AboutPage = () => (
                                 <li><a title="Link opens in a new window." href="https://github.com/marcysutton" target="_blank" rel="noopener noreferrer">github<span className="visually-hidden"> Opens in a new window</span></a></li>
                                 <li><a title="Link opens in a new window." href="http://www.linkedin.com/in/marcysutton/" target="_blank" rel="noopener noreferrer">linkedin<span className="visually-hidden"> Opens in a new window</span></a></li>
                             </ul>
-                            <Img style={{marginBottom: `1em`}} fluid={data.life.childImageSharp.fluid} alt="My shadow in front of a firepit, our van and Mt. Baker in the distance" />
                             <Img fluid={data.biking.childImageSharp.fluid} alt="My gravel bike with a safety pizza flag hanging from the seat and Bellingham Bay in the background" />
                         </aside>
                     </div>
