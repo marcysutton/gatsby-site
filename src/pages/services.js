c8c20cbcf3971c3855d086d82c0b6325af3e5164
Marcy Sutton <holla@marcysutton.com>
2020-10-19 19:29:51 -0700

    Open service page links in new windows

--- src/pages/services.js
+++ src/pages/services.js
@@ -56,10 +56,10 @@ const ServicesPage = () => (
                             <h2>In the meantime&hellip;</h2>
                             <p>I have some accessibility training available online. Check out these resources:</p>
                             <ul>
-                                <li>Smashing Magazine workshop: <a href="https://smashingconf.com/online-workshops/workshops/marcy-sutton-nov">Frontend Accessibility Masterclass</a>.<br /><strong>Starts Nov. 6-7, 2020! (Sold Out)</strong></li>
-                                <li>Frontend Masters video course: <a href="https://frontendmasters.com/courses/javascript-accessibility/">Accessibility in JavaScript Applications</a></li>
-                                <li>Egghead video course: <a href="https://egghead.io/courses/start-building-accessible-web-applications-today">Start Building Accessible Web Applications Today</a></li>
-                                <li>Accessibility chapter in <a href="https://www.smashingmagazine.com/2018/09/smashing-book-6-release/">Smashing Book 6: New Frontiers in Web Design</a></li>
+                                <li>Smashing Magazine workshop: <a href="https://smashingconf.com/online-workshops/workshops/marcy-sutton-nov" target="_blank" rel="noopener noreferrer" title="Opens in a new window">Frontend Accessibility Masterclass</a>.<br /><strong>Starts Nov. 6-7, 2020! (Sold Out)</strong></li>
+                                <li>Frontend Masters video course: <a href="https://frontendmasters.com/courses/javascript-accessibility/" target="_blank" rel="noopener noreferrer" title="Opens in a new window">Accessibility in JavaScript Applications</a></li>
+                                <li>Egghead video course: <a href="https://egghead.io/courses/start-building-accessible-web-applications-today" target="_blank" rel="noopener noreferrer" title="Opens in a new window">Start Building Accessible Web Applications Today</a></li>
+                                <li>Accessibility chapter in <a href="https://www.smashingmagazine.com/2018/09/smashing-book-6-release/" target="_blank" rel="noopener noreferrer" title="Opens in a new window">Smashing Book 6: New Frontiers in Web Design</a></li>
                                 <li><Link to="/web-accessibility-resources">Additional accessibility resources</Link></li>
                             </ul>
                             <h2>Some recent articles</h2>
@@ -67,7 +67,7 @@ const ServicesPage = () => (
                                 <li><Link to="/outsider-leverage-accessibility/">Outsider Leverage & Accessibility</Link></li>
                                 <li><Link to="/live-coding-accessibility/">Live Coding Accessibility</Link></li>
                                 <li><Link to="/links-vs-buttons-in-modern-web-applications">Links vs. Buttons in Modern Web Applications</Link></li>
-                                <li><a href="https://www.gatsbyjs.com/blog/2019-07-11-user-testing-accessible-client-routing/">User testing of accessible client-side routing techniques with Fable Tech Labs (Gatsby blog)</a></li>
+                                <li><a href="https://www.gatsbyjs.com/blog/2019-07-11-user-testing-accessible-client-routing/" target="_blank" rel="noopener noreferrer" title="Opens in a new window">User testing of accessible client-side routing techniques with Fable Tech Labs (Gatsby blog)</a></li>
                             </ul>
                         </aside>
                     </div>
